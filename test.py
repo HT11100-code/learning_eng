1f4da2fb70ba620492352c0851d8e700fae98487
haruto <takahashiharuto@TakahashinoMacBook-Pro.local>
2025-06-25 15:24:43 +0900

    Generating files

--- test.py
+++ test.py
@@ -1,13 +1,19 @@
 import fitz
 import re
+import requests
 
 doc = fitz.open("pdf/ironman.pdf")
 page = doc[0]
 text = page.get_text()
-pattern1 = r'[a-zA-Z]+'
-patern2 = r'[^\']'
+pattern1 = r'[a-zA-Z]{2,}'
+patern2 = r''
 words = re.findall(pattern1, text)
 found_words = sorted(set(word.lower() for word in words))
 
-print(f'英単語リスト: {found_words[:]}')
+file_path = "output_file"
+with open(file_path, "w") as file:
+    for word in found_words:
+        file.write(word + "\n")
+
+#print(f'英単語リスト: {found_words[:]}')
 print(f'単語数: {len(found_words)}')

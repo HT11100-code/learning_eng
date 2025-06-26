42413876eeaeb08ded08103a9f93c6753367b961
haruto <takahashiharuto@TakahashinoMacBook-Pro.local>
2025-06-27 00:16:23 +0900

    processing the data

--- test.py
+++ test.py
@@ -11,10 +11,10 @@ patern2 = r''
 words = re.findall(pattern1, text)
 found_words = sorted(set(word.lower() for word in words))
 
-file_path = "output_file"
+file_path = "output_file.txt"
 with open(file_path, "w") as file:
     for word in found_words:
         file.write(word + "\n")
 
-#print(f'英単語リスト: {found_words[:]}')
+print(f'英単語リスト: {found_words[:]}')
 print(f'単語数: {len(found_words)}')

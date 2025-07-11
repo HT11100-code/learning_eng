2e4f3e48597b718b8a9f48a95376d313e8b15d71
haruto <takahashiharuto@TakahashinoMacBook-Pro.local>
2025-07-11 17:13:12 +0900

    Update extract word

--- dev/extract_text/main_test.py
+++ dev/extract_text/main_test.py
@@ -6,17 +6,18 @@ nltk.download("punkt")
 path = "text_files/ironman.txt"
 
 with open(path)as f:
-    text = f.read()
-    
+    text = f.read()    
 sentences = nltk.sent_tokenize(text)
 
 for sent in sentences:
         print(f'{sent}')
+print('----------------------------------------------')
 
+row_wordlist = nltk.word_tokenize(text)
+eng_wordlist = []
+for word in row_wordlist:
+      if re.match(r'[a-zA-Z]', word):
+            eng_wordlist.append(word)
     
-eng_pattern1 = re.compile(r'[a-zA-Z]+')
-
-row_word = nltk.word_tokenize(text)
-    
-unique_word = set(row_word)
-print(f'Words: {sorted(unique_word)}\n Num of words: {len(unique_word)}')
+unique_wordlist = sorted(set(eng_wordlist))
+print(f'Words: {unique_wordlist}\nNum of words: {len(unique_wordlist)}')

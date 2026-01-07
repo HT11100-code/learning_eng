79d5b6c5c15f268d63ff716d1dba558e7d067913
haruto <takahashiharuto@TakahashinoMacBook-Pro.local>
2026-01-07 23:58:52 +0900

    add app and streamlit

--- dev/extract_text/processor.py
+++ dev/extract_text/processor.py
@@ -25,16 +25,4 @@ class textExtractor:
         return sorted(uniqueWords)
     
     def get_tagged_words(self, sentence):
-        # 文字列をトークン化してから品詞タグ付けを行う
-        tokens = nltk.word_tokenize(sentence)
-        tagged_tokens = nltk.pos_tag(tokens)
-        tag_dict = {"J": wordnet.ADJ,
-                    "N": wordnet.NOUN,
-                    "V": wordnet.VERB,
-                    "R": wordnet.ADV}
-        results = []
-        for word, tag in tagged_tokens:
-            wn_tag = tag_dict.get(tag[0].upper(),wordnet.NOUN)
-            results.append((word, wn_tag))
-        return results
         

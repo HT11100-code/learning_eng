16599570a9765c7c2b61eb3662838fe5be3e1ee9
jssk73322@gmail.com <jssk73322@gmail.com>
2026-01-25 02:05:49 +0900

    word2image latest

--- dev/spaCy_test/spaCy_test_main.py
+++ dev/spaCy_test/spaCy_test_main.py
@@ -3,9 +3,9 @@ from spaCy_test_source import Learningtextprocessor
 import json
 
 if __name__ == "__main__":
-    text = pathlib.Path("text_files/ironman.txt").read_text()
+    text = pathlib.Path("./text_files/demo.txt").read_text()
     processor = Learningtextprocessor()
     structured_data = processor.process_text(text)
 
-    with open("JSON_files/structured_data.json", "w", encoding="utf-8") as f:
+    with open("./JSON_files/demo_data.json", "w", encoding="utf-8") as f:
         json.dump(structured_data, f, ensure_ascii=False, indent=4)
---
+++ dev/spaCy_test/spaCy_word2image_with_data.py
@@ -0,0 +1,4 @@
+from spaCy_test_source import Learningtextprocessor
+import pathlib
+import requests
+
